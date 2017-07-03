33d3207eb13a447bb61bac7860e4610e7894ea61
Roy Choo <roychoo@hotmail.com>
2017-07-04 00:40:46 +0800

    using another user on the build vm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ssh ubuntu@ec2-13-228-78-172.ap-southeast-1.compute.amazonaws.com
+ssh roychoo@ec2-13-228-78-172.ap-southeast-1.compute.amazonaws.com
 cd url-shortener
 git pull origin master
+npm i
